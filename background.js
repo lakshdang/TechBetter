5655d0e3bb32facb388768b9bd299fe4a2a37142
Laksh <laksh.dang@gmail.com>
2019-10-16 17:37:52 +0530

    Toggle button updates data in storage

--- background.js
+++ background.js
@@ -1,16 +1,33 @@
 //Main Background script
-
 'use strict';
 
 var sites = ['stackoverflow.com', "www.w3schools.com"];
-// sites.append()
 var workTimerActive = false;
 console.log(chrome.runtime.id);
+
+var test = [];
+
+var settings = {
+	"blockedsites": sites,
+	"youtube":{
+		"Video": ["1L0TKZQcUtA"],
+		"Playlist":["PLrAXtmErZgOeiKm4sgNOknGvNjby9efdf"],
+		"Channel":[]
+	}
+}
+
+function blockYoutubeUrl(url){
+	var params = new URLSearchParams(url);
+	// console.log(params);
+}
 
 function blockUrl(url){
 	var host = new URL(url).host;
 	if(sites.indexOf(host)>-1)
 		return true;
+	// if(host.indexOf("youtube.com")>-1)
+		// return blockYoutubeUrl(url);
+	blockYoutubeUrl(url);
 	return false;
 }
 
@@ -34,8 +51,12 @@ chrome.runtime.onInstalled.addListener(function(){
 });
 
 function updateSiteList(sites){
-	chrome.storage.sync.set({'sites':sites}, function(){
+	// var stringify = JSON.stringify(settings);
+	chrome.storage.sync.set({"settings":settings}, function(){
 		console.log("Sites list saved");
+		chrome.storage.sync.get(["settings"], function(result){
+        	console.log(result.settings);
+        });
 	});
 };
 
@@ -62,17 +83,31 @@ chrome.alarms.onAlarm.addListener(function(alarm){
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse){
-    console.log(request.message);
- });
+    if(request.reason == "toggleResource")
+    	console.log(request);
+});
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
 	blockTab(tab);
 });
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-	if(changeInfo.url != undefined || changeInfo.status=="complete" || changeInfo.title != undefined){
-		// console.log(changeInfo);
-		// console.log("Sending add button message")
-		chrome.tabs.sendMessage(tabId, {url: tab.url, Listener: "youtubeListener"});
+	var hostIsYoutube = new URL(tab.url).host == "www.youtube.com";
+	if(hostIsYoutube && (changeInfo.url != undefined || changeInfo.status=="complete" || changeInfo.title != undefined)){
+	// if(hostIsYoutube && changeInfo.status=="complete"){
+		chrome.tabs.sendMessage(tabId, {url: tab.url, listener: "youtubeListener", settings: settings});
 	}
 });
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+	var hostIsYoutube = new URL(tab.url).host == "www.youtube.com";
+	if(hostIsYoutube && changeInfo.url!=undefined)
+		chrome.tabs.reload(tabId);
+});
+
+function isResourceWhitelisted(resourceType, resourceId){
+	if(resourceType == "Video")return settings.youtube.Video.indexOf(resourceId)>-1;
+	if(resourceType == "Playlist")return settings.youtube.Playlist.indexOf(resourceId)>-1;
+	if(resourceType == "Channel")return settings.youtube.Channel.indexOf(resourceId)>-1;
+	return false;
+}
--- youtubeContent.js
+++ youtubeContent.js
@@ -1,82 +1,86 @@
-console.log("Content Script Started");
-// addButton();
-var urlParams = new URLSearchParams(window.location.search);
-// console.log(urlParams.has("v"));
-// console.log(urlParams.has("list"))
-// console.log(window.location.pathname);
-// var canonical = "";
+console.log("Youtube Content Script Started");
 
+var buttonInsertDivs = ["ytd-playlist-sidebar-primary-info-renderer", "ytd-playlist-panel-renderer", "app-header-layout"]
+
+getYoutubeSettings(function(settings){
+	console.log(settings)
+})
+// var observer = new MutationObserver(function(mutations){
+// 	mutations.forEach(function(mutation) {
+//     	if(mutation.addedNodes.length==0)return;
+//     	for(var i = 0; i < mutation.addedNodes.length; i++){
+//     		if(mutation.addedNodes[i] instanceof HTMLElement){
+//     			for(var j=0; j<buttonInsertDivs.length; j++){
+//     				if(mutation.addedNodes[i].getElementsByTagName(buttonInsertDivs[j]).length>0){
+//     					// console.log(mutation.addedNodes[i].getElementsByTagName(buttonInsertDivs[j])[0]);
+//     					getSettings(addButtons);
+//     				}
+//     			}
+//     		}
+//   		}
+// 	});
+// });
+
+// observer.observe(document.body, {
+//     childList: true
+//   , subtree: true
+//   , attributes: false
+//   , characterData: false
+// })
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse){
-    // console.log(sender.tab ?
-    //             "from a content script:" + sender.tab.url :
-    //             "from the extension");
-    addVideoButton(request);
-    addPlaylistButton(request);
-    addChannelButton(request);
+  	if(request.listener == "youtubeListener"){
+  		addVideoButton(request);
+    	addPlaylistButton(request);
+    	addChannelButton(request);
+    	// console.log(request.settings);
+  	}
  });
 
-// chrome.runtime.sendMessage({message: "Message from youtubeContent script"});
-
-function addButton(request){
-	var extensionBtn = document.getElementById("Extension-Video-Id");
-	var menu = document.getElementById("info");
-	if(menu == null)
-		return;
-	var btn = document.createElement("BUTTON");
-	btn.setAttribute("id", "Extension-Video-Id");
-	btn.innerHTML = request.url;
-	btn.onclick = function(){
-		chrome.runtime.sendMessage({message: "Message from youtubeContent script"});
-	}
-	if(extensionBtn != null){
-		extensionBtn.parentNode.removeChild(extensionBtn);
-	}
-	menu.append(btn);
+function addButtons(request){
+	addVideoButton(request);
+	addPlaylistButton(request);
+	addChannelButton(request);
 }
 
 function addVideoButton(request){
 	var urlParams = new URLSearchParams(window.location.search);
 	if(urlParams.has("v")){
-		var extensionBtn = document.getElementById("Extension-Video-Id");
 		var menu = document.getElementById("info");
-		if(menu == null)
-			return;
-		var btn = document.createElement("BUTTON");
-		btn.setAttribute("id", "Extension-Video-Id");
-		btn.innerHTML = "Toggle Video";
-		btn.onclick = function(){
-			chrome.runtime.sendMessage({message: "Message from youtubeContent script"});
-		}
-		if(extensionBtn != null){
-			extensionBtn.parentNode.removeChild(extensionBtn);
-		}
-		menu.append(btn);
+		insertButton("Video", menu, request);
+		// getVideoChannelId();
 	}
 }
 
 function addPlaylistButton(request){
-	var isPlaylistVideo = (new URLSearchParams(window.location.search)).has("list");
+	var isPlaylistVideo = (new URLSearchParams(window.location.search)).has("list") && (new URLSearchParams(window.location.search)).has("v");
 	var isPlaylistPage = (window.location.pathname.indexOf("/playlist") == 0);
-	if(isPlaylistVideo || isPlaylistPage)console.log("On playlist page");
-	if(isPlaylistPage){
-		var menu = document.getElementsByTagName("ytd-playlist-sidebar-primary-info-renderer")[0].getElementsByTagName("h1")[0];
-		console.log(menu);
-	}
-	if(isPlaylistVideo){
-		var menu = document.getElementsByTagName("ytd-playlist-panel-renderer")[0].getElementsByTagName("ytd-menu-renderer")[0];
-		console.log(menu);
-	}
-	
 
+	if(isPlaylistVideo){
+		var menu = document.getElementsByTagName("ytd-playlist-panel-renderer")[0];
+		insertButton("Playlist", menu, request);
+	}
+	if(isPlaylistPage){
+		var menu = document.getElementsByTagName("ytd-playlist-sidebar-primary-info-renderer")[0];
+		insertButton("Playlist", menu, request);
+	}
+
+	// console.log(document.getElementById("TechBetterButton-Playlist"));
 }
 
 function addChannelButton(request){
 	var isChannelPage = window.location.pathname.indexOf("/channel") == 0;
 	var isUserPage = window.location.pathname.indexOf("/user") == 0
-	if(isChannelPage || isUserPage)console.log(getCanonical());
+	if(!(isChannelPage || isUserPage))return;
+	// console.log(getCanonical());
+	var menu = document.getElementById("inner-header-container");
+	insertButton("Channel", menu, request);
+}
 
+function getVideoChannelId(){
+	var channelElement = document.getElementsByTagName("ytd-video-owner-renderer")[0].getElementsByTagName("ytd-channel-name")[0].getElementsByTagName('a')[0];
+	return channelElement.getAttribute("href");
 }
 
 function getCanonical(){
@@ -85,8 +89,115 @@ function getCanonical(){
 	for (var i = 0; i < links.length; i ++) {
 	    if (links[i].getAttribute("rel") === "canonical") {
 	        canonical = links[i].getAttribute("href");
-	        break;
+	        return canonical;
 	    }
 	}
-	return canonical;
+	return window.location.href;	
+}
+
+function extractChannelId(){
+	// console.log(canonical);
+	var arr = getCanonical().split("/");
+	return arr[arr.length-1];
+}
+
+function insertButton(resourceType, menu, request){
+	var buttonId = "TechBetterButton-"+resourceType
+	var extensionBtn = document.getElementById(buttonId);
+	if(menu == undefined || menu == null)
+		return;
+	var btn = document.createElement("BUTTON");
+	btn.setAttribute("id", buttonId);
+	getInitialToggleState(request, resourceType, function(initialToggleState){
+		btn.setAttribute("toggleState", initialToggleState);
+		btn.innerHTML = btn.getAttribute("toggleState") + " " + resourceType;
+		btn.onclick = function(){
+			toggleBtnHTML(btn, resourceType);
+		}
+		if(extensionBtn != null){
+			extensionBtn.parentNode.removeChild(extensionBtn);
+		}
+		menu.append(btn);
+	})
+
+}
+
+function updateButtonState(request, resourceType, state){
+	getToggleState(request, resourceType);
+	document.getElementById("TechBetterButton-"+resourceType).innerHTML = state + " " + resourceType;
+}
+
+function getInitialToggleState(request, resourceType, callback){
+	var params = new URLSearchParams(window.location.search)
+	var resourceId = getResourceId(resourceType);
+	getYoutubeSettings(function(settings){
+		var whitelist = settings.youtube[resourceType];
+		if(whitelist.indexOf(resourceId)>-1)callback("Blacklist");
+		else callback("Whitelist");
+	});
+}
+
+function toggleBtnHTML(btn, resourceType){
+	var currState = btn.getAttribute("toggleState");
+	resourceId = getResourceId(resourceType);
+	// console.log(currVal);
+	// if(currState=="Whitelist")
+	// 	btn.setAttribute("toggleState", "Blacklist");
+	// else 
+	// 	btn.setAttribute("toggleState", "Whitelist");
+	// btn.innerHTML = btn.getAttribute("toggleState") + " " + resourceType;
+	// chrome.runtime.sendMessage({reason:"toggleResource", resourceType:resourceType, resourceId: resourceId, currState: currState});
+	
+	getYoutubeSettings(function(settings){
+		youtubeSettings = settings.youtube;
+		// console.log(youtubeSettings[resourceType])
+		removeResource(youtubeSettings[resourceType], resourceId);
+		if(currState=="Whitelist")
+			youtubeSettings[resourceType].push(resourceId);
+		
+		updateYoutubeSettings(settings, function(){			
+			if(currState=="Whitelist")
+				btn.setAttribute("toggleState", "Blacklist");
+			else 
+				btn.setAttribute("toggleState", "Whitelist");
+
+			btn.innerHTML = btn.getAttribute("toggleState") + " " + resourceType;
+			getYoutubeSettings(function(result){
+				console.log(settings.youtube);
+			})
+		});
+
+		// console.log(youtubeSettings[resourceType])
+		// console.log(settings);
+	})
+}
+
+function getResourceId(resourceType){
+	var params = new URLSearchParams(window.location.search)
+
+	if(resourceType=='Video')
+		return params.get("v");
+	if(resourceType=='Playlist')
+		return params.get("list");
+	if(resourceType=='Channel')
+		return extractChannelId();
+	return null;
+}
+
+function getYoutubeSettings(callback){
+	chrome.storage.sync.get(["settings"], function(result){
+		callback(result.settings)
+	});
+}
+
+function updateYoutubeSettings(settings, callback){
+	chrome.storage.sync.set({"settings": settings},function(){
+		callback();
+	});
+}
+
+function removeResource(resourceWhiteList, resourceId){
+	var index = resourceWhiteList.indexOf(resourceId);
+	if (index > -1) 
+		resourceWhiteList.splice(index, 1);
 }
